e0b74235ca264e839700ecafa1706df347f99249
Rohan das <77046452+SURD1618@users.noreply.github.com>
2024-01-31 16:26:21 +0530

    Update SearchView.js

--- src/components/SearchView.js
+++ src/components/SearchView.js
@@ -20,7 +20,7 @@ const MovieCard = ({movie}) => {
 }
 
 const SearchView = ({ keyword, searchResults }) => {
-//   const title = searchResults.length > 0 ? `You are searching for ${keyword}` : `No results found for for ${keyword}`;
+    
     const title = `You are searching for ${keyword}`;
 
     const resultsHtml = searchResults.map((obj, i) => { 
@@ -28,6 +28,7 @@ const SearchView = ({ keyword, searchResults }) => {
     });
 
     const sadDogImageUrl = `https://cdn.dribbble.com/users/29245/screenshots/2480165/media/392ffcb86abdd6d9eb8f6c6c8ffe629f.jpg?resize=800x600&vertical=center`;
+    
     return (
         <>
             <Hero text={title} />
@@ -59,31 +60,3 @@ const SearchView = ({ keyword, searchResults }) => {
 };
 
 export default SearchView;
-
-
-
-// https://api.themoviedb.org/3/search/movie?query=star%20wars&include_adult=false&language=en-US&page=1'
-//   API KEY = 36c2ab884dfcc4966187fec1be3fc44a
-// EXAMPLE LINK= https://api.themoviedb.org/3/keyword/36c2ab884dfcc4966187fec1be3fc44a/movies?include_adult=false&language=en-US&page=1'
-
-
-
-//NOTE 1
-// The warning you're encountering, Warning: validateDOMNesting(...): 
-// <li> cannot appear as a descendant of <li>., suggests an issue with the structure of your HTML elements. 
-//     In your SearchView component, you are rendering a list item (<li>) directly inside another list item, which is not allowed in HTML.
-
-// Let's correct the structure of your resultsHtml variable. 
-// It seems like you want to display a list of movie titles. You should place the <li> elements 
-//     directly inside an unordered or ordered list (<ul> or <ol>).
-//   <div key={i}>{obj.original_title}</div>        
-// <li key={obj.id}>{obj.original_title}</li>             //NOTE  
-
-
-
-
-//NOTE 2
-// If multiple movies have the same original_title, you could end up with duplicate keys. 
-// To address this, consider using a unique identifier, such as the movie's ID from the API response
-// By using the obj.id as the key, you ensure that each key is unique within the list. 
-// Make sure that the property you choose as the key is unique for each item in the searchResults array.
